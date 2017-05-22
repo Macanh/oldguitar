a977daf8d260b04e190fc1f40acf8f2164627b49
Macanh <mactheanh460568@gmail.com>
2017-05-23 03:47:51 +0700

    Update Chord.js

--- Chord.js
+++ Chord.js
@@ -23,12 +23,6 @@ var chords = [
 		type: 'm',
 	},
 	{
-        chord: ['x', 0, 2, 2, 2, 3],
-        fingering: ['x', 0, 1, 1, 1, 2],
-        name: 'A',
-        type: '7',
-    },
-    {
         chord: ['x', 0, 2, 2, 2, 0],
         fingering: ['x', 0, 1, 2, 3, 0],
         name: 'A',
